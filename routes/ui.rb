0906f118ded60a8eb17b0fb3c6084930071ee14c
William DeFreese <alexdefreese@gmail.com>
2014-12-24 12:35:26 -0500

    Controller logic for UI

--- routes/ui.rb
+++ routes/ui.rb
@@ -3,6 +3,22 @@ module Sinatra
 		module Routing
 			module UI
 				def self.registered(app) do
+					app.get '/agents' do
+						@agents = Agent.all(order: [:updated_at.asc])
+					end
+
+					app.get '/agents/:id' do
+						@agent = Agent.first(id: params[:id])
+					end
+
+					app.post '/agents/:id/task' do
+						@agent = Agent.first(id: params[:id])
+						@task = Task.create(input: params[:input], open: true, agent: @agent)
+					end
+
+					app.get '/agents/:id/tasks.json' do
+
+					end
 				end
 			end
 		end
